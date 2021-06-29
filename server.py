f4ebb14c3bcbd252367f23a5f139877bfd7f65bd
Soxoj <soxoj@protonmail.com>
2021-06-30 01:04:52 +0300

    Added JSON with site data

--- server.py
+++ server.py
@@ -1,47 +1,80 @@
 #!/usr/bin/env python3
+import sys
+
 from aiohttp import web
 
 __version__ = "0.0.1"
 
 
-def make_answer(res=None, err: str=''):
-    data = {
-        'maigret-adapter': __version__,
-    }
+class MaigretAdapterServer:
+    def __init__(self, addr):
+        self.addr = addr
+        self.services = {}
 
-    if res and err:
-        raise ValueError
+    def make_answer(self, res=None, err: str=''):
+        data = {
+            'maigret-adapter': __version__,
+        }
 
-    if res:
-        data.update({'result': res})
-    elif err:
-        data.update({'error': err})
+        if res and err:
+            raise ValueError
 
-    return data
+        if res:
+            data.update({'result': res})
+        elif err:
+            data.update({'error': err})
 
+        return data
 
-async def status(request):
-    return web.Response(text=f'Maigret adapter v{__version__}')
+    def register_service(self, name, *args, **kwargs):
+        data = {}
+        data.update(kwargs)
+        self.services[name] = data
 
+    async def status(self, request):
+        return web.Response(text=f'Maigret adapter v{__version__}')
 
-async def check(request):
-    result = {}
+    async def make_data_json(self, request):
+        result = {}
+        for name, data in self.services.items():
+            result[name] = {
+                'tags': data.get('tags'),
+                'urlMain': data.get('url'),
+                'url': f'{self.addr}/check/{name}/{{username}}',
+            }
+        return web.json_response({'sites': result})
 
-    service = request.match_info.get('service')
-    identifier = request.match_info.get('identifier')
+    async def check(self, request):
+        result = {}
 
-    if not identifier:
-        result = make_answer(err='No identifier was specified')
-    elif not service:
-        result = make_answer(err='No service was specified')
-    else:
-        result = make_answer(res={'status': f'checking {identifier} through {service}'})
+        service = request.match_info.get('service')
+        identifier = request.match_info.get('identifier')
 
-    return web.json_response(result)
+        if not identifier:
+            result = self.make_answer(err='No identifier was specified')
+        elif not service:
+            result = self.make_answer(err='No service was specified')
+        else:
+            result = self.make_answer(res={'status': f'checking {identifier} through {service}'})
+
+        return web.json_response(result)
+
+    def start(self, debug=False):
+        app = web.Application()
+
+        routes = [
+            web.get('/', self.status),
+            web.get('/maigret_data.json', self.make_data_json),
+            web.get('/check/{service}/{identifier}', self.check),
+        ]
+        app.add_routes(routes)
+        if debug:
+            app.add_routes([web.get('/exit', lambda _: sys.exit(0))])
+        web.run_app(app)
 
 
 if __name__ == '__main__':
-    app = web.Application()
-    app.add_routes([web.get('/', status),
-                    web.get('/check/{service}/{identifier}', check)])
-    web.run_app(app)
+    address = 'http://localhost:8080'
+    server = MaigretAdapterServer(address)
+    server.register_service('example', tags=['tag'], url='http://example.com')
+    server.start(debug=True)
